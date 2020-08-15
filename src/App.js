cb63c21833376fae93dae4167707749a6fda5610
Darryl King <darrylking15@gmail.com>
2020-08-14 21:05:32 -0400

    step1-working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Auth from './Components/Auth'
 import Dashboard from './Components/Dashboard'
 import Form from './Components/Form'
 import Nav from './Components/Nav'
 import Post from './Components/Post'
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Auth/>
-      <Dashboard/>
-      <Form/>
-      <Post/>
+     <Nav/>
+     <Switch>
+      <Route exact path='/' component={Auth}/>
+      <Route path='/dashboard' component={Dashboard}/>
+      <Route path='/post/:postid' component={Post}/>
+      <Route path='/new' component={Form}/>
+      
+    </Switch>
     </div>
   );
 }
---
+++ src/Components/routes.js
@@ -0,0 +1,8 @@
+import{ Switch, Route} from "react-router-dom";
+import Auth from './Auth'; 
+import Dashboard from "./Dashboard";
+import Form from "./Dashboard";
+import Post from "./Dashboard";
+
+
+
